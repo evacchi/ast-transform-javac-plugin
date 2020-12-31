027e550ba401a790bd42f17c8b55e495359e1b36
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-12-31 12:32:33 +0100

    Transormation working

--- src/main/java/io/github/evacchi/javac/plugin/MyPlugin.java
+++ src/main/java/io/github/evacchi/javac/plugin/MyPlugin.java
@@ -1,16 +1,10 @@
 package io.github.evacchi.javac.plugin;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.MemberSelectTree;
 import com.sun.source.tree.MethodInvocationTree;
-import com.sun.source.tree.MethodTree;
-import com.sun.source.tree.ReturnTree;
-import com.sun.source.tree.StatementTree;
-import com.sun.source.tree.VariableTree;
 import com.sun.source.util.JavacTask;
 import com.sun.source.util.Plugin;
 import com.sun.source.util.TaskEvent;
@@ -25,6 +19,9 @@ import com.sun.tools.javac.util.Names;
 
 public class MyPlugin implements Plugin {
 
+    private TreeMaker treeMaker;
+    private Names names;
+
     @Override
     public String getName() {
         return "MyPlugin";
@@ -36,6 +33,8 @@ public class MyPlugin implements Plugin {
         Context context = ((BasicJavacTask) task).getContext();
         Log.instance(context)
                 .printRawLines(Log.WriterKind.NOTICE, "Hello from " + getName());
+        treeMaker = TreeMaker.instance(context);
+        names = Names.instance(context);
 
         task.addTaskListener(new TaskListener() {
             @Override
@@ -50,30 +49,17 @@ public class MyPlugin implements Plugin {
                     return;
                 }
                 e.getCompilationUnit().accept(new TreeScanner<Void, Void>() {
+
                     @Override
-                    public Void visitMethod(MethodTree method, Void v) {
-                        TreeMaker treeMaker = TreeMaker.instance(context);
-                        Names names = Names.instance(context);
-
-                        StatementTree statementTree = method.getBody().getStatements().get(0);
-
-                        if (!(statementTree instanceof ReturnTree)) return null;
-
-                        ReturnTree returnTree = (ReturnTree) statementTree;
-                        ExpressionTree expression = returnTree.getExpression();
-                        MethodInvocationTree mi = (MethodInvocationTree) expression;
+                    public Void visitMethodInvocation(MethodInvocationTree mi, Void unused) {
                         MemberSelectTree m = (MemberSelectTree) mi.getMethodSelect();
                         if (m.getIdentifier().contentEquals("filter")) {
-                            List<? extends ExpressionTree> arguments = mi.getArguments();
-                            JCTree.JCMethodInvocation index = treeMaker.Apply(com.sun.tools.javac.util.List.nil(),
-                                                                              treeMaker.Ident(names.fromString("index")),
-                                                                              (com.sun.tools.javac.util.List<JCTree.JCExpression>) arguments);
-                            ((JCTree.JCMethodInvocation) mi).args = com.sun.tools.javac.util.List.of(index);
+                            injectIndex(mi, treeMaker, names);
                         }
-
 
                         return null;
                     }
+
                 }, null);
             }
         });
@@ -81,21 +67,13 @@ public class MyPlugin implements Plugin {
 
 //
     }
-    private static JCTree.JCIf createCheck(VariableTree parameter, Context context) {
-        TreeMaker factory = TreeMaker.instance(context);
-        Names symbolsTable = Names.instance(context);
 
-//        return factory.at(((JCTree) parameter).pos)
-//                .If(factory.Parens(createIfCondition(factory, symbolsTable, parameter)),
-//                    createIfBlock(factory, symbolsTable, parameter),
-//                    null);
-
-        return null;
+    private void injectIndex(MethodInvocationTree mi, TreeMaker treeMaker, Names names) {
+        List<? extends ExpressionTree> arguments = mi.getArguments();
+        JCTree.JCMethodInvocation index = treeMaker.Apply(com.sun.tools.javac.util.List.nil(),
+                                                          treeMaker.Ident(names.fromString("index")),
+                                                          (com.sun.tools.javac.util.List<JCTree.JCExpression>) arguments);
+        ((JCTree.JCMethodInvocation) mi).args = com.sun.tools.javac.util.List.of(index);
     }
 
-    private void addCheck(MethodTree method, VariableTree parameter, Context context) {
-        JCTree.JCIf check = createCheck(parameter, context);
-        JCTree.JCBlock body = (JCTree.JCBlock) method.getBody();
-        body.stats = body.stats.prepend(check);
-    }
 }
